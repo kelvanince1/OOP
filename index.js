18be590ed55ea1dd0bae6f490775c10cfbe2eea0
kelvanince1 <incekelvan@gmail.com>
2018-07-03 09:08:34 -0500

    Add factory

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  draw: () => {
-    console.log('draw');
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1
+//   },
+//   draw: () => {
+//     console.log('draw');
+//   }
+// };
+
+function createCircle(radius) {
+  return {
+    radius: radius,
+    draw: () => {
+      console.log('draw');
+    }
   }
 };
 
+const circle = createCircle(1);
 circle.draw();
