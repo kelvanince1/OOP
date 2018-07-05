128a4c963be5d1adf9d2687186bef5008c38e194
kelvanince1 <incekelvan@gmail.com>
2018-07-05 07:27:30 -0500

    Add Stopwatch function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OOP
+
+Repo containing some OOP programming practices in JavaScript which can be re-used. 
--- index.js
+++ index.js
@@ -20,3 +20,29 @@ function createCircle(radius) {
 
 const circle = createCircle(1);
 circle.draw();
+
+function Circle(radius) {
+  this.radius = radius;
+  let defaultLocation = { x: 0, y: 0};
+  // Define a function which can return the private variable defaultLocation and make it readable
+  this.getDefaultLocation = function() {
+    return defaultLocation;
+  };
+  this.draw = () => {
+    console.log('draw');
+  };
+  Object.defineProperty(this, 'defaultLocation', {
+    get: function() {
+      return defaultLocation;
+    },
+    set: function(value) {
+      if (!value.x || !value.y) {
+        throw new Error('Invalid');
+      }
+      defaultLocation = value;
+    }
+  });
+};
+
+const another = new Circle(1);
+another.location = { x: 1 };
