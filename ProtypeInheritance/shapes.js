7de82da581ad721367e075c4e9bd80012f48c011
Kelvan Ince <incekelvan@gmail.com>
2018-07-08 15:51:08 -0500

    Add a super constructor to allow data passed to children to be handled dynamically and set the contrcutor of the Circle function back to its own constructor instead of the Shapes prototype

--- ProtypeInheritance/shapes.js
+++ ProtypeInheritance/shapes.js
@@ -1,7 +1,8 @@
 // Prototypical inheritance in defaultLocation
 
 // Define a Shape function which all functions that are a type of shape, defined at some point later, can inherit from
-function Shape() {
+function Shape(color) {
+  this.color = color;
 };
 
 // Place a method on the Shape prototype
@@ -10,11 +11,15 @@ Shape.prototype.duplicate = function() {
 };
 
 // Define a type of shape which can inherit from the Shape constructor
+// Use the call method on the Shape constructor to pull the color property down and allow it to be made mutatable to the Circle function
 function Circle(radius) {
+  Shape.call(this, color);
+
   this.radius = radius;
 };
 
 // Use the create method on the Circles Object to re-create its prototype using Shapes prototype
 Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
 
 const c = new Circle(1);
